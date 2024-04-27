2a3e0f37fa4611ef15e33b2abee795d6aa1e3b5b
Taran Patel <tppatel4@wisc.edu>
2024-04-27 03:02:33 +0000

    Midweek

--- BackendDeveloperTests.java
+++ BackendDeveloperTests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -10,6 +11,16 @@ public class BackendDeveloperTests {
 
   GraphADT<String, Double> graph = new GraphPlaceholder();
   BackendInterface backend = new Backend(graph);
+  /**
+   * This method launches the JavaFX application that you would like to test
+   * BeforeEach of your @Test methods are run.  Copy and paste this into your
+   * own Test class, and change the SampleApp.class reference to instead
+   * refer to your own application class that extends Application.
+   */
+  @BeforeEach
+  public void setup() throws Exception {
+    Frontend.setBackend(new BackendPlaceholder(new GraphPlaceholder()));
+  }
 
   /**
    * Tests the method getListOfAllLocations().
@@ -84,5 +95,45 @@ public class BackendDeveloperTests {
     }
     assertTrue(thrown);
 
+  }
+
+  /**
+   * This test has the program find all locations that are within a specified time from the starting location. This test clicks on
+   * the findLocations button and verifies that it displays the correct list of locations.
+   */
+  @Test
+  public void IntergrationTestReachableLocations() {
+    Label locationsLabel = lookup("#reachableLocationsLabel").query();
+
+    clickOn("#startingLocationField");
+    write("Computer Sciences and Statistics");
+    clickOn("#timeField");
+    write("200");
+    clickOn("#findLocationsButton");
+    assertEquals("Locations within 200 seconds of Computer Sciences and Statistics\n\tAtmospheric, Oceanic and Space Sciences\n\tMemorial Union",locationsLabel.getText());
+  }
+
+  /**
+   * This test tests when the checkbox is clicked before the "Find Shortest Path" button is clicked. This test verifies that the
+   * program displays an error message telling the user to use the "Find Shortest Path" button before the checkbox.
+   */
+  @Test
+  public void IntegrationTestCheckboxBeforeShortestPath() {
+    Label shortestPathLabel = lookup("#shortestPathLabel").query();
+
+    clickOn("#startPathField");
+    write("Union South");
+    clickOn("#endPathField");
+    write("Atmospheric, Oceanic and Space Sciences");
+    clickOn("#walkingTimesCheckbox");
+    assertEquals("Cannot use the checkbox when a shortest path\nhas not been found.", shortestPathLabel.getText());
+  }
+
+  /**
+   * This method starts up our Frontend class.
+   */
+  public static void main(String[] args) {
+    Frontend.setBackend(new BackendPlaceholder(new GraphPlaceholder()));
+    Application.launch(Frontend.class);
   }
 }
